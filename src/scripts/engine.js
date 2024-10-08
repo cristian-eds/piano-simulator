be9a48e9fa0a84e72834e3255648027614939ce5
Cristian <cristian.santos0308@gmail.com>
2024-10-08 16:14:18 -0300

    Adicionado controle para mostrar ou não tecla chave para cada tecla

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
 let audio = new Audio("src/tunes/a.wav");
 let mapedKeys = [];
@@ -27,4 +28,11 @@ const handleVolume = (e) => {
     audio.volume = e.target.value;
 }
 
+const showHideKeys = () => {
+    pianoKeys.forEach(key => {
+        key.classList.toggle("hide");
+    })
+}
+
 volumeSlider.addEventListener("input", handleVolume);
+keysCheck.addEventListener("click", showHideKeys);
